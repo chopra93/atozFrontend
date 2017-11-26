8879e69ec1c3fb6efb94fe290ccf36a1b761303c
Amit Chopra <amitchopra.iiit@gmail.com>
2017-11-27 00:54:34 +0800

    modification

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,16 +1,30 @@
-// myApp.controller('emiController', function($scope, $http) {
-//     $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/atoz/v1/emi?p='+$scope.principal+'&r='+$scope.rate+'&t='+$scope.duration+'&type='+$scope.monthOrYear).
-//         success(function(response) {
-//             $scope.emiResponse = response.data;
-//         });
-// });
-
 var myApp = angular.module('atoz', []);
 myApp.controller('atozController', ['$scope', '$http', function($scope, $http) {
 
 $scope.emi = function(){
   $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/atoz/v1/emi?p='+$scope.principal+'&r='+$scope.rate+'&t='+$scope.duration+'&type='+$scope.monthOrYear).success(function(response){
     $scope.emiResponse = response;
+    console.log(response);
+  });  
+};
+
+$scope.fd = function(principal,rate,duration,monthOrYear,compoundingFreq){
+  $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/v1/fd?p='+principal+'&r='+rate+'&t='+duration+'&type='+monthOrYear+'&cType='+compoundingFreq).success(function(response){
+    $scope.fdResponse = response;
+    console.log(response);
+  });  
+};
+
+$scope.rd = function(principal,rate,duration,monthOrYear,compoundingFreq){
+  $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/v1/rd?p='+principal+'&r='+rate+'&t='+duration+'&type='+monthOrYear+'&cType='+compoundingFreq).success(function(response){
+    $scope.rdResponse = response;
+    console.log(response);
+  });  
+};
+
+$scope.pincodeDetail = function(pincode){
+  $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/v1/pincode?pincode='+pincode).success(function(response){
+    $scope.pincodeResponse = response;
     console.log(response);
   });  
 };
@@ -50,41 +64,5 @@ $scope.branchDetail = function(bankId,stateId,districtId,cityId){
   });
 };
 
-
-// var refresh = function(){
-//   $http.get('/contactlist').success(function(response){
-//     $scope.contactlist = response;
-//   });
-// };
-
-// refresh();
-
-// $scope.addContact = function(){
-//   $http.post('/contactlist', $scope.contact).success(function(response){
-//     refresh(); //refreshing page
-//   });
-// };
-
-// $scope.edit = function(id){
-//   $http.get('/contactlist/' + id).success(function(response){
-//     $scope.contact = response;
-//   });
-// };  
-
-// $scope.update = function(){
-//   $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
-//     refresh();
-//   });
-// };
-
-// $scope.remove = function(id){
-//   $http.delete('/contactlist/'+id).success(function(response){
-//     refresh();
-//   });
-// };
-
-// $scope.deselect = function(){
-//   $scope.contact = "";
-// }
 
 }]);﻿
