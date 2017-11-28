0fe3f08f1bff13e52a6ac09d30498ca25a05cb69
Amit Chopra <amitchopra.iiit@gmail.com>
2017-11-28 09:05:25 +0800

    modification 2

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -9,21 +9,21 @@ $scope.emi = function(){
 };
 
 $scope.fd = function(principal,rate,duration,monthOrYear,compoundingFreq){
-  $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/v1/fd?p='+principal+'&r='+rate+'&t='+duration+'&type='+monthOrYear+'&cType='+compoundingFreq).success(function(response){
+  $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/atoz/v1/fd?p='+principal+'&r='+rate+'&t='+duration+'&type='+monthOrYear+'&cType='+compoundingFreq).success(function(response){
     $scope.fdResponse = response;
     console.log(response);
   });  
 };
 
 $scope.rd = function(principal,rate,duration,monthOrYear,compoundingFreq){
-  $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/v1/rd?p='+principal+'&r='+rate+'&t='+duration+'&type='+monthOrYear+'&cType='+compoundingFreq).success(function(response){
+  $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/atoz/v1/rd?p='+principal+'&r='+rate+'&t='+duration+'&type='+monthOrYear+'&cType='+compoundingFreq).success(function(response){
     $scope.rdResponse = response;
     console.log(response);
   });  
 };
 
 $scope.pincodeDetail = function(pincode){
-  $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/v1/pincode?pincode='+pincode).success(function(response){
+  $http.get('http://ec2-34-216-106-44.us-west-2.compute.amazonaws.com:8080/atoz/v1/pincode?pincode='+pincode).success(function(response){
     $scope.pincodeResponse = response;
     console.log(response);
   });  
